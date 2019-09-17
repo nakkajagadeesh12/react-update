cfc31fd16a41d4c07973bd5d87eeed6c278157d0
nakkajagadeesh12 <nakkajagadeesh1234@gmail.com>
2019-09-17 18:08:28 +0530

    2 files modified

--- display.js
+++ display.js
@@ -1,12 +1,35 @@
 import React from 'react'
 
-const Display = ({history,location}) =>{
-  console.log(history,"history");
+const Display = ({ history, location }) => {
+  const ar = [{ id: 1, version: "16.8", data: { id: 2, name: 'Bhaskar', technology: "react" } }];
   return (
     <div>
-    display the details
+      {
+        ar.map(item => {
+          console.log(Object.entries(ar), "entries")
+        })
+      }
     </div>
   )
 }
 
 export default Display;
+
+
+// {
+//   ar.map(item => {
+//     return (
+//       <div>{
+//         Object.keys(item).map(key => {
+//           return <div>{key === 'data' ?
+//             Object.keys(item[key]).map(k => {
+//               console.log(k, "k");
+//               return <div>{item[key][k]}</div>
+//             })
+//             : item[key]}</div>
+//         })
+//       }
+//       </div>
+//     )
+//   })
+// }
